d1537b8a9415a38c31d82cd5468ec780661abf72
Matthew Schneier <mschneier7@gmail.com>
2020-07-02 18:09:22 -0500

    More routes.

--- network/models.py
+++ network/models.py
@@ -3,4 +3,11 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField(User)
+    followed_by = models.ManyToManyField(User)
+
+class Post(models.Model):
+    content = models.TextField(default="post")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+    date = models.DateTimeField(auto_now_add=True)
--- network/views.py
+++ network/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from .models import User
+from .models import *
 
 
 def index(request):
@@ -61,3 +63,32 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+
+@login_required
+def newPost(request):
+    if request.method == "POST":
+        content = request.POST["content"]
+        user = request.user.id
+        likes = 0
+        post = Post.objects.create(content, user, likes)
+        messages.success(request, "Post created.")
+        return redirect("/profile")
+    return render(request, "network/newPost.html")
+        
+
+@login_required
+def allPosts(request):
+    posts = Post.objects.order_by("date")
+    return render(request, "network/allPosts.html")
+
+
+@login_required
+def profile(request):
+    user = User.objects.filter(id=request.user.id)
+    posts = Post.objects.filter(user=request.user.id)
+    following = user.following
+    followed_by = user.followed_by
+    return render(request, "network/profile.html", {
+        "posts": posts, "following": following, "followed_by": followed_by
+    })
